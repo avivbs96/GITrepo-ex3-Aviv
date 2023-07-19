959cf1b44983d03f876d64338a7f63d98a243dc7
avivbs96 <85108235+avivbs96@users.noreply.github.com>
2023-07-19 14:38:13 +0300

    Update to Aviv new name while printed

--- Aviv.java
+++ Aviv.java
@@ -1,7 +1,7 @@
 public class Aviv {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Aviv " + i);
+            System.out.println("Aviv New name" + i);
         }
     }
 }
